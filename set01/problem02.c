14b5b2c325722f80434b71c403674f0900656d9e
Khushi 	 Naruka <119475072+khushi2302@users.noreply.github.com>
2022-12-09 14:26:55 +0000

    answer

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-int main() {    
-
-    int number1, number2, sum;
-    
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-
-    // calculating sum
-    sum = number1 + number2;      
-    
-    printf("%d + %d = %d", number1, number2, sum);
-    return 0;
-} 
-
+int main()
+{
+  int number1,number2,sum;
+  printf("enter two integer");
+  scanf("%d,%d",&number1,&number2);
+  //calculating sum
+  sum=number1=number2;
+  printf("%d+%d=%d", number1, number2,sum);
+  return 0;
+}
