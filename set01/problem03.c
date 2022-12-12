03502764f53bab82f42947f846e94baa6949250c
Khushi 	 Naruka <119475072+khushi2302@users.noreply.github.com>
2022-12-12 07:35:47 +0000

    answer

--- set01/problem03.c
+++ set01/problem03.c
@@ -4,10 +4,13 @@ int main(){
 
 	int num1, num2, result;
 
-	/*Accept the number from the user*/
+	/*Accept the numbers from the user*/
 	printf("\nEnter the two number: ");
 	scanf("%d %d", &num1, &num2);
-  
+
+	/* Pass the value of num1 and num2 as parameter to function add.
+	    The value returned is stored in the variable result
+	 */
 	result = add(num1, num2);
 	printf("\nAddition of %d and %d is %d", num1, num2, result);
 
@@ -19,6 +22,4 @@ int add(int no1, int no2)
 {
 	int res;
 	res = no1 + no2;
-	return res;
-
-  }
+}
--- set01/problem04.c
+++ set01/problem04.c
@@ -1,21 +1,20 @@
-#include<stdio.h>
-long addTwoNumbers(long*,long*);
-int main
+#include <stdio.h>
+float addTwoNumbers(float *,float *);
+ 
+int main()
 {
-  long fno,sno,sum;
-  printf("\n\n pointer: add two number using call by reference:\n");
-  printf("----------------------------------------------------:\n");
-  printf("input the first number:");
-  scanf("%1d",&fno);
-  printf("input the second number:");
-  scanf("%1d",&sno);
-  sum=addTwoNumbers(&fno,sno);
-  printf("the sum of %1d and %d is %1d\n\n",fno,sno,sum);
-  return0;
+   float fno, sno, sum;   
+ 
+   printf("Input the first number:");
+   scanf("%f", &fno);
+   printf("Input the second  number:");
+   scanf("%f",&sno);   
+   sum = addTwoNumbers(&fno, &sno);
+   printf("The sum of %.0f and %.0f  is %.0f", fno,sno, sum);
+   return 0;
 }
-long addTwoNumber(long*n1,long*n2)
+float addTwoNumbers(float*n1, float*n2) 
 {
-  long sum;
-  sum=*n1+*n2)
-    return sum;
+   float sum;
+   sum=*n1+*n2;
 }
